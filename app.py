6e7c0ee0c0401b876d924ddbf00ee182ff666330
Alberta Williams <alberta04@gmail.com>
2017-09-15 23:36:18 -0400

    adds comments

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from data import data
 from movie import Movie
 
 movies = []
+
+# initialize list of movies
 for movie in data:
 	movies.append(Movie(movie))
 
+""" load movies into web page and open browser"""
 open_movies_page(movies)
 	
--- movie.py
+++ movie.py
@@ -1,6 +1,9 @@
 
 class Movie():
+	"""This class models a movie object"""
+
 	def __init__(self, data):
+		"""The constructor requires an object to initialize data attributes""" 
 		self.title = data['title']
 		self.storyline = data['storyline']
 		self.poster_image_url = data['poster_image_url']
